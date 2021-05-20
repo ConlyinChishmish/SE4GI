8a1b6140009db137a3d745c029f9dee78624f3a6
ConlyinChishmish <83450257+ConlyinChishmish@users.noreply.github.com>
2021-05-20 11:43:49 +0200

    Update BinEcoCode.py

--- IMPLEMENTATION/BinEcoCode.py
+++ IMPLEMENTATION/BinEcoCode.py
@@ -1,1 +1,42 @@
-this is a file in a folder
+from flask import (
+    Flask, render_template, request, redirect, flash, url_for, session, g
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from werkzeug.exceptions import abort
+
+from psycopg2 import (
+        connect
+)
+
+
+# Create the application instance
+app = Flask(__name__, template_folder="templates")
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
+
+def get_dbConn():
+    if 'dbConn' not in g:
+        myFile = open('dbConfig.txt')
+        connStr = myFile.readline()
+        g.dbConn = connect(connStr)
+    
+    return g.dbConn
+
+def close_dbConn():
+    if 'dbConn' in g:
+        g.dbComm.close()
+        g.pop('dbConn')
+        
+        
+ #Add your function here
+        
+        
+        
+        
+        
+        
+        
+        if __name__ == '__main__':
+    app.run(debug=True)
