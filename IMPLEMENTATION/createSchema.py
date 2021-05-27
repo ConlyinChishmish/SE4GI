3ab51517fc578afb47eef85224aab8e1c5b5bb43
Elisa849417 <elisa.servidio@mail.polimi.it>
2021-05-27 17:17:43 +0200

    Updated createSchema.py

--- IMPLEMENTATION/createSchema.py
+++ IMPLEMENTATION/createSchema.py
@@ -9,11 +9,22 @@ from psycopg2 import (
 )
 import requests
 import json
+import datetime
 
 #open the configuration parameter from a txt file the table
 myFile = open('dbConfig.txt')
 connStr = myFile.readline()
 myFile.close()
+
+
+cleanup = (
+        'DROP TABLE IF EXISTS pa_user CASCADE',
+        'DROP TABLE IF EXISTS comments',
+        'DROP TABLE IF EXISTS bin_cairns',
+        'DROP TABLE IF EXISTS bin_townsville',
+        'DROP TABLE IF EXISTS litter'
+        )
+
 
 #variable list containing the structures of the database
 commands = (
@@ -27,88 +38,64 @@ commands = (
         )
         """,
     
+        #table of comments
+        """ 
+        CREATE TABLE comments (
+                comment_id SERIAL PRIMARY KEY,
+                author_id VARCHAR(5),
+                created TIMESTAMP DEFAULT NOW(),
+                title VARCHAR(350) NOT NULL,
+                body VARCHAR(500) NOT NULL,
+                FOREIGN KEY (author_id)
+                    REFERENCES pa_user (postal_code)
+        )
+        """,
+    
         #table of bins
         """ 
-        CREATE TABLE bin(
+        CREATE TABLE bin_cairns(
                 bin_id SERIAL PRIMARY KEY,
-                bin_date TIMESTAMP DEFAULT NOW(),
+                bin_date DATE DEFAULT NOW(),
                 lon DOUBLE PRECISION NOT NULL,
                 lat DOUBLE PRECISION NOT NULL,
                 infographic BOOLEAN NOT NULL DEFAULT 'False',
-                infographic_date TIMESTAMP DEFAULT NOW(),
-                geom geometry(POINT) /* i'm not sure of the correctness of this line*/
+                infographic_date DATE DEFAULT NULL,
+                geom geometry(POINT)
         )
         """,
     
-    
-    # table of the gardbage collector
+        #table of bins
         """ 
-            CREATE TABLE gardbage_collector(
-                personal_code SERIAL PRIMARY KEY,
-                username VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL
+        CREATE TABLE bin_townsville(
+                bin_id SERIAL PRIMARY KEY,
+                bin_date DATE DEFAULT NOW(),
+                lon DOUBLE PRECISION NOT NULL,
+                lat DOUBLE PRECISION NOT NULL,
+                infographic BOOLEAN NOT NULL DEFAULT 'False',
+                infographic_date DATE DEFAULT NULL,
+                geom geometry(POINT)
         )
-        """,        
-        #table of the status of the bin
-        """ 
-            CREATE TABLE bin_status(
-                bin_id INTEGER UNIQUE NOT NULL,
-                GC_code INTEGER UNIQUE NOT NULL,
-                date TIMESTAMP DEFAULT NOW(),
-                overfull BOOLEAN NOT NULL DEFAULT 'False',
-                PRIMARY KEY(bin_id, GC_code),
-                
-                CONSTRAINT fk_bin
-                    FOREIGN KEY(bin_id)
-                        REFERENCES bin(bin_id)
-                        ON DELETE SET NULL,
-                CONSTRAINT fk_gc
-                    FOREIGN KEY(GC_code)
-                        REFERENCES gardbage_collector(personal_code)
-                        ON DELETE SET NULL,
-        )
-        """,
-        
-        
-        
+        """
         )
 
 #create the connection with the database
 conn = connect(connStr)
 cur = conn.cursor()
+for command in cleanup :
+    cur.execute(command)
 for command in commands :
     cur.execute(command)
 cur.close()
 conn.commit()
 conn.close()
 
-#IMPORT DATA ABOUT MUNICIPALITIES
-# create the table containing a list of municipalities and their postal code
-#this table will be used to verify the correctness of the username during the registration of the PA
-
 #setup db connection (generic connection path to be update with your credentials: 'postgresql://user:password@localhost:5432/mydatabase')
-engine = create_engine('postgresql://postgres:r3df0x@localhost:5432/binecoDB') 
+engine = create_engine('postgresql://postgres:jawadamp@localhost:5432/binecoDB') 
 
-# creating the dataframe of the municipalities 
-# data obtained from http://lab.comuni italiani.it/download/comuni.html
-# !!NOTE: i'm using the municipality of italy because i can't find a list of australian city id
+#IMPORT DATA FROM EPICOLLECT
+# send the request to the API of Epicollect5
+response = requests.get('https://five.epicollect.net/api/export/entries/bineco-web-application?per_page=100')
 
-#opening the file and save it in a daframe
-fileTxt = open("data/listacomuni.txt")
-df_patemp = pd.read_csv(fileTxt,sep=';')
-fileTxt.close()
-               
-#selecting only the usefull columns
-df_pa = df_patemp[['Comune', 'Provincia', 'CAP']]
-
-# write the dataframe into postgreSQL
-df_pa.to_sql('pa_data', engine, if_exists = 'replace', index=False)
-
-# CREATING THE TABLE OF THE LITTER BY USING EPICOLLECT5 DATA
-#connecting to the API of epicollect5
-response = requests.get('https://five.epicollect.net/api/export/entries/bineco-web-application')
-
-# store the raw text of the response in a variable
 raw_data = response.text
 
 # parse the raw text response 
@@ -117,61 +104,94 @@ data = json.loads(raw_data)
 # from JSON to Pandas DataFrame
 data_df = pd.json_normalize(data['data']['entries'])
 len(data_df) # for a good plot it's better to extract more then 50 lines
+# preprocessing data_df
+data_df = data_df.iloc[: ,4:13].copy()
+data_df.columns = ['Date_of_creation','Time_of_creation','Lytter_type',
+                     'Quantity','Type_of_infrastructure','Comment','Photo','Latitude',
+                     'Longitude']
 
 # from Pandas DataFrame to GeoPandas GeoDataFrame
 #we add a geometry column using the numeric coordinate colums
-
-lon = '3_Position.longitude'#NOTE they are already numeric coordinate columns 
-lat = '3_Position.latitude'
-data_geodf = gpd.GeoDataFrame(data_df, geometry=gpd.points_from_xy(data_df[lon], data_df[lat]))
+data_geodf = gpd.GeoDataFrame(data_df, geometry=gpd.points_from_xy(data_df.Longitude, data_df.Latitude))
 
 # write the dataframe into postgreSQL
-data_geodf.to_sql('litter', engine, if_exists = 'replace', index=False)
+data_geodf.to_postgis('litter', engine, if_exists = 'replace', index=False)
 
 #CREATING THE DATAFRAME OF THE BIN USING THE DATE FROM OSM AND UPDATE THE TABLE OF BINS
-# creating the dataframe of the bins
-# data obtained from OSM
 
+#Town of CAIRNS
 #opening the file and save it in a daframe
-filegeojson = open("data/Waste_basket_Cairns.geojson")
-bin_df = gpd.read_file(filegeojson)
-fileTxt.close()
-               
+filegeojson_cairns = open("data/Waste_basket_Cairns.geojson")
+binCairns_gdf = gpd.read_file(filegeojson_cairns)
+filegeojson_cairns.close()
 #extract the usefull columns
-bin_df = bin_df['full_id','geometry']
+binCairns_gdf = binCairns_gdf[['full_id','geometry']]
+#rename id
+for i in range(len(binCairns_gdf)):
+    binCairns_gdf.loc[i,'full_id'] = i
 # create the columns of longitude and latitude from the geometry attribute
-bin_df['lon'] = df['geometry'].x
-bin_df['lat'] = df['geometry'].y
+binCairns_gdf['lon'] = binCairns_gdf['geometry'].x
+binCairns_gdf['lat'] = binCairns_gdf['geometry'].y
+# create the columns of datetime and set it
+binCairns_gdf['date'] = datetime.datetime(2018, 5, 1)
+#import to PostgreSQL
+binCairns_gdf.to_postgis('cairns_temp', engine, if_exists = 'replace', index=False)
 
-# Copy the dataframe to keep the original intact
-insert_gdf = bin_df_C.copy()
+#Town of TOWNSVILLE
+#opening the file and save it in a daframe
+filegeojson_townsville = open("data/Waste_basket_Townsville.geojson")
+binTownsville_gdf = gpd.read_file(filegeojson_townsville)
+filegeojson_townsville.close()
+#extract the usefull columns
+binTownsville_gdf = binTownsville_gdf[['full_id','geometry']]
+#rename id 
+for i in range(len(binTownsville_gdf)):
+    binTownsville_gdf.loc[i,'full_id'] = i
+# create the columns of longitude and latitude from the geometry attribute
+binTownsville_gdf['lon'] = binTownsville_gdf['geometry'].x
+binTownsville_gdf['lat'] = binTownsville_gdf['geometry'].y
+# create the columns of datetime and set it
+binTownsville_gdf['date'] = datetime.datetime(2018, 5, 1)
+#import in PostgreSQL
+binTownsville_gdf.to_postgis('townsville_temp', engine, if_exists = 'replace', index=False)
 
-# Make a new field containing the WKB dumped from the geometry column, then turn it into a regular 
-#this way should be faster 
-insert_gdf["geom_wkb"] = insert_gdf["geometry"].apply(lambda x: wkb_dumps(x))
-
-# creating the query command
-insert_query = """
-    INSERT INTO bin (bin_id,lon,lat, geom)
-    VALUES (%(full_id)s,%(lon)s,%(lat)s, ST_GeomFromWKB(%(geom_wkb)s));
-"""
-#creating a list of parameters to be inserted into values
-params_list = [
-    {
-        "full_id": i,
-        "lon": row["lon"],
-        "lat": row["lat"],
-        "geom_wkb": row["geom_wkb"]
-    } for i, row in insert_gdf.iterrows()
-]
+# creating the query command to insert data into DB original bins' tables from temporary bins' table
+insert_query = ("""
+    INSERT INTO bin_cairns (bin_id,lon,lat,geom,bin_date)
+    SELECT full_id,lon,lat,geometry,date FROM cairns_temp;
+    """,
+    """
+    INSERT INTO bin_townsville (bin_id,lon,lat,geom,bin_date)
+    SELECT full_id,lon,lat,geometry,date FROM townsville_temp;
+    """)
+# drop temporary bins' tables
+cleanup_temp = (
+        'DROP TABLE IF EXISTS cairns_temp',
+        'DROP TABLE IF EXISTS townsville_temp'
+        )
 
 # Connect to the database and make a cursor
 conn = connect(connStr)
 cur = conn.cursor()
 
-# Iterate through the list of execution parameters and apply them to an execution of the insert query
-for params in params_list:
-    cur.execute(insert_query, params)
+# Execution of the insert query and clean up of temporary tables
+for command in insert_query :
+    cur.execute(command)
+for command in cleanup_temp :
+    cur.execute(command)
+    
 cur.close()
 conn.commit()
 conn.close()
+
+#CREATING DATAFRAME FOR MUNICIPALITIES CORRESPONDING POSTCODES IN ALL AUSTRALIA
+#opening the file and save it in a daframe
+fileTxt = open("data/df_australia_postcode.csv")
+df_au_postcode = pd.read_csv(fileTxt,sep=',')
+fileTxt.close()
+
+#select useful columns
+df_au_postcode = df_au_postcode.iloc[:, 2:4]
+
+#import to PostgreSQL
+df_au_postcode.to_sql('pa_data', engine, if_exists = 'replace', index=False)
