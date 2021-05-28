9b6c6986632bf6e82d6798efc647987653517a0d
Doromilla <76112915+Doromilla@users.noreply.github.com>
2021-05-28 23:38:24 +0200

    working indexlogged.html(page for logged user)

--- IMPLEMENTATION/BinEcoCode.py
+++ IMPLEMENTATION/BinEcoCode.py
@@ -7,7 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.exceptions import abort
 
 from psycopg2 import (
-        connect
+    connect
 )
 
 from shapely.geometry import Point
@@ -17,22 +17,25 @@ app = Flask(__name__, template_folder="templates")
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
 
+
 def get_dbConn():
     if 'dbConn' not in g:
         myFile = open('dbConfig.txt')
         connStr = myFile.readline()
         g.dbConn = connect(connStr)
-    
+
     return g.dbConn
+
 
 def close_dbConn():
     if 'dbConn' in g:
         g.dbComm.close()
         g.pop('dbConn')
-        
-        
- #Add your function here
- #registration
+
+ # Add your function here
+ # registration
+
+
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -50,28 +53,30 @@ def register():
         else:
             conn = get_dbConn()
             cur = conn.cursor()
-            cur.execute('SELECT postal_code FROM pa_user WHERE postal_code = %s', (postal_code,))
+            cur.execute(
+                'SELECT postal_code FROM pa_user WHERE postal_code = %s', (postal_code,))
             if cur.fetchone() is not None:
                 error = 'User {} is already registered.'.format(postal_code)
                 cur.close()
             else:
-                cur.execute('SELECT postal_code FROM pa_data WHERE postal_code = %s', (postal_code,))
+                cur.execute(
+                    'SELECT postal_code FROM pa_data WHERE postal_code = %s', (postal_code,))
                 if cur.fetchone() is None:
                     error = 'User {} does not exist'.format(postal_code)
                     cur.close()
-                #else:
+                # else:
                  # cur.execute('SELECT pa_data.locality FROM pa_data WHERE postal_code = %s', (postal_code,))
-                  #if (cur.fetchone() != municipality):
-					#error=cur.fetchone()
+                  # if (cur.fetchone() != municipality):
+                    # error=cur.fetchone()
                     #error = '{} and {} do not correspond'.format(postal_code,municipality)
-                     #cur.close()
+                    # cur.close()
 
         if error is None:
-            conn = get_dbConn
+            conn = get_dbConn()
             cur = conn.cursor()
             cur.execute(
                 'INSERT INTO pa_user (postal_code,municipality,password) VALUES (%s, %s, %s)',
-                (postal_code,municipality, generate_password_hash(password))
+                (postal_code, municipality, generate_password_hash(password))
             )
             cur.close()
             conn.commit()
@@ -80,13 +85,16 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
- 
-#login
+
+# login
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         postal_code = request.form['postal_code']
         password = request.form['password']
+
         conn = get_dbConn()
         cur = conn.cursor()
         error = None
@@ -96,124 +104,131 @@ def login():
         user = cur.fetchone()
         cur.close()
         conn.commit()
-        
+
         if user is None:
             error = 'Incorrect postal code.'
-        elif not check_password_hash(user[2],password):
+        elif not check_password_hash(user[2], password):
             error = 'Incorrect password.'
-        
+
         if error is None:
             session.clear()
             session['user_id'] = user[0]
             return redirect(url_for('index'))
         flash(error)
-    
+
     return render_template('auth/login.html')
 
-#logout
+# logout
+
+
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
     session.clear()
     return redirect(url_for('index'))
- 
-# "cookies"
-def load_logged_in_user():
-    postal_code = session.get('postal_code')
 
-    if postal_code is None:
+# "cookies"
+
+
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
         g.user = None
     else:
         conn = get_dbConn()
         cur = conn.cursor()
         cur.execute(
-            'SELECT * FROM pa_user WHERE postal_code = %s', (postal_code,)
+            'SELECT * FROM pa_user WHERE postal_code = %s', (user_id,)
         )
         g.user = cur.fetchone()
         cur.close()
         conn.commit()
-        conn_close()
+        conn.close()
     if g.user is None:
         return False
-    else: 
+    else:
         return True
-    
+
 # Create a URL route in our application for "/"
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     if load_logged_in_user():
-        return render_template('index.html')
+        return render_template('indexlogged.html')
     else:
-        return render_template('about.html')
+        return render_template('index.html')
 
 
 # UC.3 Pa enters new data about the bin
 @app.route('/newBin', methods=('GET', 'POST'))
 def new_bin():
-    if request.method == 'POST' :
+    if request.method == 'POST':
         lon = request.form['lon']
         lat = request.form['lat']
         infographic = request.form['infographic']
-        
-        geom = Point(lon,lat)    
+
+        geom = Point(lon, lat)
         error = None
-       
+
         # check if the data inserted are correct
         if (not lon or not lat):
             error = '*this data is required!'
-        elif (float(lat)<-90 or float(lat)>90):
-            error ='Please insert a valid value for the latitude -90<= lat <=90'
-        elif(float(lon)<0 or float(lon)>=360):
-            error ='Please insert a valid value for the longitude 0<= lon <360'
-         
-        #check if something went wrong in compiling the form  
-        if error is not None :
+        elif (float(lat) < -90 or float(lat) > 90):
+            error = 'Please insert a valid value for the latitude -90<= lat <=90'
+        elif(float(lon) < 0 or float(lon) >= 360):
+            error = 'Please insert a valid value for the longitude 0<= lon <360'
+
+        # check if something went wrong in compiling the form
+        if error is not None:
             flash(error)
             return redirect(url_for('new_bin'))
-        #everything in the form is ok, database connection is allowed
-        else : 
+        # everything in the form is ok, database connection is allowed
+        else:
             conn = get_dbConn()
             cur = conn.cursor()
-            cur.execute('INSERT INTO bin (lon, lat, infographic, geom ) VALUES (%f, %f, %s, ST_Point(%(geom)s))', 
+            cur.execute('INSERT INTO bin (lon, lat, infographic, geom ) VALUES (%f, %f, %s, ST_Point(%(geom)s))',
                         (lon, lat, infographic, geom)
                         )
             cur.close()
             conn.commit()
             return redirect(url_for('index'))
-    else :
-        return render_template('blog/newBin.html')       
-        
-        
+    else:
+        return render_template('blog/newBin.html')
+
+
 @app.route('/create_comment', methods=('GET', 'POST'))
 def create_comment():
     if load_logged_in_user():
-        if request.method == 'POST' :
+        if request.method == 'POST':
             title = request.form['title']
             body = request.form['body']
             error = None
-            
-            if not title :
+
+            if not title:
                 error = 'Title is required!'
-            if error is not None :
+            if error is not None:
                 flash(error)
                 return redirect(url_for('create_comment'))
-            else : 
+            else:
                 conn = get_dbConn()
                 cur = conn.cursor()
-                cur.execute('INSERT INTO post (title, body, author_id) VALUES (%s, %s, %s)', 
+                cur.execute('INSERT INTO post (title, body, author_id) VALUES (%s, %s, %s)',
                             (title, body, g.user[0])
                             )
                 cur.close()
                 conn.commit()
                 return redirect(url_for('index'))
-        else :
+        else:
             return render_template('blog/createComment.html')
-    else :
+    else:
         error = 'Only loggedin users can insert comments!'
         flash(error)
         return redirect(url_for('login'))
-   
+
+
 def get_comment(id):
     conn = get_dbConn()
     cur = conn.cursor()
@@ -229,49 +244,51 @@ def get_comment(id):
         abort(404, "Comment id {0} doesn't exist.".format(id))
 
     if comment[1] != g.user[0]:
-        abort(403)  #access is forbidden 
+        abort(403)  # access is forbidden
 
     return comment
+
 
 @app.route('/<int:id>/update_comment', methods=('GET', 'POST'))
 def update_comment(id):
     if load_logged_in_user():
         comment = get_comment(id)
-        if request.method == 'POST' :
+        if request.method == 'POST':
             title = request.form['title']
             body = request.form['body']
             error = None
-            
-            if not title :
+
+            if not title:
                 error = 'Title is required!'
-            if error is not None :
+            if error is not None:
                 flash(error)
                 return redirect(url_for('update_comment'))
-            else : 
+            else:
                 conn = get_dbConn()
                 cur = conn.cursor()
                 cur.execute('UPDATE comment SET title = %s, body = %s'
-                               'WHERE comment_id = %s', 
-                               (title, body, id)
-                               )
+                            'WHERE comment_id = %s',
+                            (title, body, id)
+                            )
                 cur.close()
                 conn.commit()
                 return redirect(url_for('index'))
-        else :
-            return render_template('blog/updateComment.html', comment = comment)
-    else :
+        else:
+            return render_template('blog/updateComment.html', comment=comment)
+    else:
         error = 'Only loggedin users can insert comments!'
         flash(error)
         return redirect(url_for('login'))
 
+
 @app.route('/<int:id>/delete_comment', methods=('POST',))
 def delete_comment(id):
-    conn = get_dbConn()                
+    conn = get_dbConn()
     cur = conn.cursor()
     cur.execute('DELETE FROM comments WHERE comment_id = %s', (id,))
     conn.commit()
-    return redirect(url_for('index'))        
-        
-        
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
