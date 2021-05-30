3f73e8cb3576337b6ea8cb4520dd3a63d2da2a2b
safari1996 <83756849+safari1996@users.noreply.github.com>
2021-05-30 18:07:06 +0200

    Add files via upload

---
+++ query.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 17:45:07 2021
+
+@author: arya
+"""
+#querybyarea
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+def get_dbConn():
+conn = get_dbConn()
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT Litter_type* FROM db_Conn")
+
+myresult = mycursor.fetchall()
+
+for row in myresult:
+    print(row)
+    
+cur.close()   
+    
